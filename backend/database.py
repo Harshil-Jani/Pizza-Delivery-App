75c79faee2aa081fc0c042907f90bc49246cf3a3
Harshil Jani <harshiljani2002@gmail.com>
2023-12-14 15:51:17 +0530

    [backend] Secure DB with environment variables
    - Add .env file and .env.sample file.
    - Utlize python-dotenv package to load environment variables.
    
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
+DB_USER=config.get("DB_USER")
+DB_PASSWORD=config.get("DB_PASSWORD")
+DB_HOST=config.get("DB_HOST")
+DB_NAME=config.get("DB_NAME")
+
+print(config.get("DB_HOST"))
 engine = create_engine(
-    "postgresql://postgres:postgres@localhost/pizza_delivery", echo=True
+    f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}',
+    echo=True
 )
 
 Base = declarative_base()
