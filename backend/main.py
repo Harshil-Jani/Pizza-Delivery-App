274bba25b5d2414205e7c23ecdfaedf9bf6311ef
Harshil Jani <harshiljani2002@gmail.com>
2023-12-14 13:19:13 +0530

    [ci] Adding github action for code formatting
    - Add yaml script for code formatting.
    - committing formatted file.
    
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,11 @@ from order_routes import order_router
 
 app = FastAPI(title="Pizza Delivery Application Demo")
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 app.include_router(auth_router)
 app.include_router(order_router)
--- backend/order_routes.py
+++ backend/order_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
 
+
 @order_router.get("/")
 async def root():
     return {"message": "Hello World"}
